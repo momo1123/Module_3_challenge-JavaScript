c306bedc935aaaddfaaae0ac70ef2d848c71ccf7
Muhammad Mohsin <mmohsin@cymru.com>
2023-07-05 02:28:05 -0400

    Updating readme and script.js

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -3,183 +3,75 @@ var password_display = document.querySelector("#password");
 var lower_abc = "abcdefghijklmnopqrstuvwxyz",
 upper_abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 number_123 = "0123456789",
-sepcial_char = "!@#$%^&*()_+=-[]{}\|;:',.<>/?",
+sepcial_char = "!@#$%^&*()_+=-[]{}\|;:',.\]\][!##%*&$(*#!@#)(*&<>/?",
 password_lower_abc = "",
 password_upper_abc = "",
 password_number_123 = "",
-password_speical_char = "";
-
+password_speical_char = "",
+final_password = "";
+var all_password_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+=-[]{}|;:',.<>/?";
+// Function that with click event
 function writePassword() {
+  // Resets the passwords before generating a new password
+  password_display.value = null;
   var user_prompt = window.prompt("Plese choose the length of the password");
+    if (!user_prompt){
+      return;
+  } 
   if(user_prompt < 8  || user_prompt > 128){
     window.alert("Please select 8 - 128 caharacters for password");
     writePassword();
-  } else if (!user_prompt){
-      return;
-     }
-  console.log("Outisde of char of password");
-    // ******************TEMP**********************
-  // if (user_prompt >= 8 || user_prompt <128){
-  //   var lowercase_prompt = window.prompt("Do you want to include lowercase characters?");
-  //   if (lowercase_prompt === "yes" || lowercase_prompt === "YES"){
-  //     for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
-  //           // Lower password Loop
-  //         password_lower_abc += lower_abc.charAt(Math.floor(Math.random() * n));
-  //         password_lower_abc.includes(lower_abc);
-  //         password_display.value = password_lower_abc;
-
-  //         console.log("lower_abc #", i,password_lower_abc);
-  //         }
-  //       }
-  // } else  if (!user_prompt){
-  //   return;
-  //  }
-  // console.log("outisde of lowercase_prompt");
-    // ******************TEMP**********************
-
-  for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
-      // Lower password Loop
-      password_lower_abc += lower_abc.charAt(Math.floor(Math.random() * n));
-      // password_display.value = password_lower_abc;
-      // console.log("lower_abc #", i,password_lower_abc);
-      // Upper password Loop
-      password_upper_abc += upper_abc.charAt(Math.floor(Math.random() * n));
-      // console.log("Upper_abc #", i,password_upper_abc);
-      // Number password Loop
-      password_number_123 += number_123.charAt(Math.floor(Math.random() * n));
-      // console.log("Number_123 #", i,password_number_123);
-      // Special char Loop
-      password_speical_char += sepcial_char.charAt(Math.floor(Math.random() * n));
-      // console.log("Number_123 #", i,password_speical_char);
-
-      password_display.value = password_lower_abc + password_upper_abc + password_number_123 + password_speical_char;
   }
-  // return password_lower_abc;
-  
+// If user selects the correct amount the following statements are triggered
+  if (user_prompt >= 8 || user_prompt <128){
+  var lowercase_prompt = window.prompt("Do you want to include lowercase characters?");
+  var uppercase_prompt = window.prompt("Do you want to include uppercase characters?");
+  var number_prompt = window.prompt("Do you want to include numbers?");
+  var specialchar_prompt = window.prompt("Do you want to include special characters?");
+  // If all are yess
+  if ((lowercase_prompt === "YES" || lowercase_prompt === "yes") && (uppercase_prompt === "YES" || uppercase_prompt === "yes") &&
+  (number_prompt === "YES" || number_prompt === "yes") && (specialchar_prompt === "YES" || specialchar_prompt === "yes")){
+       for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
+    password_display.value += all_password_chars.charAt(Math.floor(Math.random() * all_password_chars.length));
+    console.log("All yes");
+    }
+  }
+  // If only lowercase is true
+  if (((lowercase_prompt === "YES" || lowercase_prompt === "yes") && (uppercase_prompt === "NO" || uppercase_prompt === "no") &&
+  (number_prompt === "NO" || number_prompt === "no") && (specialchar_prompt === "NO" || specialchar_prompt === "no"))){
+    for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
+      password_display.value += lower_abc.charAt(Math.floor(Math.random() * lower_abc.length));
+      console.log("Lowercase YES");
+    }
+  }
+  // If only upper case is true
+  if (((lowercase_prompt === "NO" || lowercase_prompt === "no") && (uppercase_prompt === "YES" || uppercase_prompt === "yes") &&
+  (number_prompt === "NO" || number_prompt === "no") && (specialchar_prompt === "NO" || specialchar_prompt === "no"))){
+    for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
+      password_display.value += upper_abc.charAt(Math.floor(Math.random() * upper_abc.length));
+      console.log("Uppercase YES");
+    }
+  }
+  // If only number is true
+  if (((lowercase_prompt === "NO" || lowercase_prompt === "no") && (uppercase_prompt === "NO" || uppercase_prompt === "no") &&
+  (number_prompt === "YES" || number_prompt === "yes") && (specialchar_prompt === "NO" || specialchar_prompt === "no"))){
+    for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
+      password_display.value += number_123.charAt(Math.floor(Math.random() * number_123.length));
+      console.log("Number YES");
+    }
+  }
+  // If only special chars is true
+  if (((lowercase_prompt === "NO" || lowercase_prompt === "no") && (uppercase_prompt === "NO" || uppercase_prompt === "no") &&
+  (number_prompt === "NO" || number_prompt === "no") && (specialchar_prompt === "YES" || specialchar_prompt === "yes"))){
+    for (var i = 0, n = user_prompt; i < user_prompt; ++i) {
+      password_display.value += sepcial_char.charAt(Math.floor(Math.random() * sepcial_char.length));
+      console.log("Special Char YES");
+      }
+    }
+  } if (((lowercase_prompt === "NO" || lowercase_prompt === "no") && (uppercase_prompt === "NO" || uppercase_prompt === "no") &&
+  (number_prompt === "NO" || number_prompt === "no") && (specialchar_prompt === "NO" || specialchar_prompt === "no"))){
+    window.alert("Please select at least one type");
+    writePassword();
+  }
 }
-
 generateBtn.addEventListener("click", writePassword);
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// // function writePassword() {
-// //   var user_prompt = window.prompt("Plese choose the length of the password");
-// //   if(user_prompt < 8  || user_prompt > 128){
-//   //     window.alert("Please select 8 - 128 caharacters for password");
-//   //     writePassword();
-//   // } else if (!user_prompt){
-//     //   return;
-//     //  }
-//     function writePassword(){
-//       // generatePassword = ("This is the current value of writePassword",writePassword)
-//   // var password = generatePassword();
-//   // console.log("this is the current value of password",password)
-//   // var passwordText = document.querySelector("#password");
-//   // console.log("this is the current value of passwordText", passwordText);
-//   // passwordText.value = password;
-
-//   var user_prompt = window.prompt("Plese choose the length of the password");
-//   if (!user_prompt){
-//       return;
-//   } 
-//   if (user_prompt < 8  || user_prompt > 128){
-//         window.alert("Please select 8 - 128 caharacters for password");
-//         writePassword();
-//         console.log("this is the if statement where the user chooses incorrect number of characters")
-//         return;
-//       } 
-//   if (user_prompt >= 8 || user_prompt <128){
-//     console.log("User chose the number", user_prompt);
-//       var lowercase_prompt = window.prompt("Do you want to include lowercase characters?");
-
-//         if (lowercase_prompt === "yes" || lowercase_prompt === "YES"){
-//           console.log("This is the lowercase yes response");
-//           var lower_abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//           for (var i = 0;
-//               i < user_prompt;
-//               i++) {
-//                 var lower_password = [lower_abc[Math.floor(Math.random() * lower_abc.length)]];
-//                 var password = lower_password;
-//                 console.log("lower_password chars: ", password);
-//           }
-//           console.log("this is the value of i", i);
-//         } else {
-//           console.log("this is the else for the lowercase user_prompt ===yes");
-//         }
-//         }
-//   if (lowercase_prompt === "yes" || lowercase_prompt === "YES"){
-//         var uppercase_prompt= window.prompt("Do you want to include uppercase characters?");
-//         if (uppercase_prompt === "yes" || uppercase_prompt === "YES"){
-//           console.log("This is the uppercase user_pormpt === yes option");
-//           var upper_abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-//           for (var i = 0;
-//             i < user_prompt;
-//             i++) {
-//               var upper_password = [upper_abc[Math.floor(Math.random() * upper_abc.length)]];
-//                password = password.concat(upper_password);
-//                console.log("l + u - chars:", password);
-//         }
-//         } else {
-//           console.log("this is the else for the uppercase user_prompt === YES");
-//         }
-//         }
-//   if (uppercase_prompt ==="yes" || uppercase_prompt ==="YES"){
-//           var num_prompt = window.prompt("Do you want to include numbers?");
-//           if (num_prompt === "yes" || num_prompt === "YES"){
-//             console.log("This is the numbers user_pormpt === yes option");
-//             var num_123 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128]
-//             for (var i = 0;
-//               i < user_prompt;
-//               i++) {
-//                 var num_password = [num_123[Math.floor(Math.random() * num_123.length)]];
-//                 password = password.concat(num_password);
-//                 console.log("l + u + n - chars", password);
-//           }
-          
-//           }else {
-//             console.log("this is the else for the numbers user_prompt ===yes");
-//           }
-//         }
-//   if (num_prompt === "yes" || num_prompt=== "YES"){
-//           var special_prompt= window.prompt("Do you want to include special characters?");
-//           if (special_prompt === "yes" || special_prompt === "YES"){
-//             console.log("This is the special characters user_pormpt === yes option");
-//             var special_char = ["`", "@", "!", "#", "$" , "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "~", "|", ";", ":", "'" ];
-//             for (var i = 0;
-//               i < user_prompt;
-//               i++) {
-//                 var special_password = [special_char[Math.floor(Math.random() * special_char.length)]];
-//                 password = password.concat(special_password);
-//                 console.log("l + u + n + s - chars", password);
-//                 for (var i = 0;
-//                      i < user_prompt;
-//                      i ++){
-//                       console.log("Password letter", i, " and password char",password[i]);
-
-//                   }
-//           }
-//           }else {
-//             console.log("this is the else for the special characters user_prompt ===yes");
-//           }
-//         } else{
-//           window.alert("This is the Else statement, the function will be return;")
-//           return;
-//         }
-
-//         // var lower_abc = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-//         // var uper_abc = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-//         // console.log("this is an array from lower_abc and upper_abc \n" +lower_abc);
-      
-      
-//   // var password = generatePassword();
-//   // var passwordText = document.querySelector("#password");
-
-//   // passwordText.value = password;
-
-
-// }
-
-// // Add event listener to generate button
-
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Module_3_challenge-JavaScript
 JavaScript Challenge
+
+This script generates a random password that asks user for various char types. In order for this to run as expected please only answer in 'YES'/'yes' or 'NO'/'no'. This script only works with 1 type of no, in other words you must say yes/YES to all except for 1 or all yes.
+
+[Please see my application in:](./index.html)
+It can also be found in: https://momo1123.github.io/Module_3_challenge-JavaScript/index.html
